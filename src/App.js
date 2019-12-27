1880791cb3c747d78008fe0d7920785da91597fd
Rev. Andi N. Fiziks <40371794+nuklearfiziks@users.noreply.github.com>
2019-12-27 21:05:05 +0000

    Knobs!

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Row from "./row";
 import TestOsc from "./util/test-osc";
 import Oscilloscope from "./oscilloscope";
 import BlueControl from "./blue-control";
+import Knob from "./knob";
 function App() {
   return (
     <Rack>
       <Row>
+        <Knob value="50" />
         <BlueControl></BlueControl>
       </Row>
     </Rack>
